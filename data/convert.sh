95794557f246f64173d38815137b097e855d92fb
David Inho Lee <davidinholee@gmail.com>
2020-12-10 11:49:36 -0500

    Remove redundant files

--- data/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd mashup-mp3
-for file in *.mp3; do
-   ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 44100 ../mashup-wav/"${file%.mp3}".wav
-cd ..
-cd original-mp3
-for file in *.mp3; do
-   ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 44100 ../original-wav/"${file%.mp3}".wav
-done
